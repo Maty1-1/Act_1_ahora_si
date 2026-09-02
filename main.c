5a4c19f2f9eaaa8c486bf3d94f8c77afb95d6726
Maty1-1 <ffrias@ucol.mx>
2026-09-02 16:20:22 -0600

    Actividad 1

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("================\n");
+    printf("!Hola git hub¡\n");
+    printf("===============\n");
+    printf("Mi nombre es: Fernando Matias\n");
+    printf("Estoy aprendiendo programacion\n");
+    printf("Este es mi primer programa\n");
+    printf("Publicado en git hub\n");
+    printf("Bienbenido a mi repositorio\n");
+    return 0;
+}
